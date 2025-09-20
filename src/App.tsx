586d0475527ae9adaa4042115a9ee340d17b59b8
atasoya <ataatasoy2013@gmail.com>
2025-09-20 17:43:39 +0200

    github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Github, GithubIcon } from "lucide-react";
 import "./App.css";
 import Board from "./components/Board";
 
 function App() {
 	return (
 		<div className="w-full h-screen bg-slate-950 flex items-center flex-col">
-			<div className="flex items-center flex-col">
-				<p className="text-3xl font-bold text-white mt-20">
+			<div className="flex items-center flex-col mt-20">
+				<a
+					className="text-white mb-2"
+					href="https://github.com/atasoya/tictactoeai"
+				>
+					<GithubIcon />
+				</a>
+				<p className="text-3xl font-bold text-white">
 					Tic Tac Toe - AI
 				</p>
 				<p className="text-white">
